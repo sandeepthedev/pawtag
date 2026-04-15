fb886dea87ac45d38ba5634b75054a683b5265a1
sandeep <sandeepsajikumar443@gmail.com>
2026-04-15 18:17:50 +0530

    feat: configure Firebase with env-based credentials, add auth and Firestore helpers

---
+++ src/firebase/auth.js
@@ -0,0 +1,18 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from "./config";
+
+const auth = getAuth(app);
+
+export const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logout = () => {
+    return signOut(auth);
+};
+
+export default auth;
